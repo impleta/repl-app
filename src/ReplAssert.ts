e8e110684100e6a6241febea0529101dd5ca50c4
impletavita <dev@impleta.com>
2023-10-20 14:03:04 -0400

    Cleaned up messages on assertions

--- src/ReplAssert.ts
+++ src/ReplAssert.ts
@@ -8,25 +8,38 @@ class ReplAssert {
   };
 
   static failureMessageHandler = (msg: string, e: Error) => {
-    console.log(msg, e);
+    console.log(msg);
     return e;
   };
 
+  /**
+   * A proxy to intercept calls to function defined in chai.assert.
+   *
+   * This is used for
+   *  1. Modifying the success and failure messages
+   *  2. Determing which asserts (by line #) failed when running in batch mode so
+   *     reports can be created appropriately.
+   *
+   * TODO:
+   *  Currently, we cannot determine line # when an assertion succeeds. One way to achieve
+   *  that is to use create hashes of 
+   */
   static createAssertProxy(obj: AssertStatic) {
     return new Proxy(obj, {
       get(target: AssertStatic, prop: keyof AssertStatic) {
         if (typeof target[prop] === 'function') {
           return new Proxy(target[prop], {
             apply: (target, thisArg, argumentsList) => {
+              const args = ReplAssert.getArguments(argumentsList);
               try {
                 const ret = Reflect.apply(target, thisArg, argumentsList);
                 ReplAssert.successMessageHandler(
-                  `Success!: assert.${prop}(${argumentsList.join()})`
+                  `Success!: assert.${prop}(${args})`
                 );
                 return ret;
               } catch (e) {
                 ReplAssert.failureMessageHandler(
-                  `Failed!: assert.${prop}(${argumentsList.join()})`,
+                  `Failed!: assert.${prop}(${args})`,
                   e as Error
                 );
               }
@@ -38,6 +51,18 @@ class ReplAssert {
       },
     });
   }
+
+  /**
+   * Returns a comma-separated string of arguments for display purposes
+   * Adds quotes if an argument is a string
+   * @param argumentsList list of arguments
+   */
+  static getArguments(argumentsList: unknown[]): string {
+    return argumentsList
+      .map(a => (typeof a === 'string' ? `'${a}'` : a))
+      .join(', ');
+  }
+
 }
 
 const replAssert = ReplAssert.createAssertProxy(assert);
