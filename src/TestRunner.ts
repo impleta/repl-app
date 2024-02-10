5aee3df672351f5f979c0eff0e39188c2e89be70
impletavita <dev@impleta.com>
2024-02-10 14:56:33 -0500

    Using typedi, supporting --report.generate
    Reports are now generated only when --report.generate is specified in the command line.
    Using typedi to avoid passing the command line args.

--- src/TestRunner.ts
+++ src/TestRunner.ts
@@ -45,6 +45,6 @@ export class TestRunner {
     const results = await Promise.all(promises);
     const testRunReport = new TestRunReport(results);
 
-    return testRunReport.getResult(this.context.CommandLineArgs);
+    return testRunReport.getResult();
   }
 }
